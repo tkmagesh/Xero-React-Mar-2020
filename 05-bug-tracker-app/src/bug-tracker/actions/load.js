32f1f281567dc6e7ae52684ca3df794ede8e941a
Magesh Kuppan <tkmagesh77@gmail.com>
2020-03-11 06:51:55 +0530

    server communication - initial

--- 05-bug-tracker-app/src/bug-tracker/actions/load.js
+++ 05-bug-tracker-app/src/bug-tracker/actions/load.js
@@ -1,9 +1,18 @@
+import axios from 'axios';
+
 function getLocalBugs(){
     var bugs = [
         {id : 1, name : 'Sample bug - 1', isClosed : false, createdAt : new Date()},
         { id: 2, name: 'Sample bug - 2', isClosed: true, createdAt: new Date() },
     ];
     return bugs;
+}
+
+function getServerBugs(){
+    axios
+        .get('http://localhost:3030/bugs')
+        .then(response => response.data)
+        .then(bugs => console.table(bugs));
 }
 export function load(){
     const bugs = getLocalBugs();
